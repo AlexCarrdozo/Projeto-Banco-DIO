6401b97eae28805be83ab376f7e9a97b5eaff69f
Alex <alex.fraga@estudante.ifto.edu.br>
2024-04-29 15:52:51 -0300

    Atualizações do Projeto banco

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner s = new Scanner(System.in);
+        String Agencia, Nome;
+
+        System.out.println("Por favor, digite o número da agência:");
+        Agencia = s.nextLine();
+
+        System.out.println("Digite o número da conta:");
+        int Numero = s.nextInt();
+        s.nextLine();
+
+        System.out.println("Digite seu nome:");
+        Nome = s.nextLine();
+
+        System.out.println("Digite seu saldo:");
+        BigDecimal Saldo = s.nextBigDecimal();
+
+        System.out.println("Olá " +Nome+", obrigado por criar uma conta em nosso banco, sua agência é "+Agencia+", conta "+Numero+" e seu saldo "+Saldo+" já está disponível para saque");
     }
 }
